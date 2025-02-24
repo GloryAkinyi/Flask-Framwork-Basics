4539369020ce6f80a045ebfeefd87a7966ca290c
gloryakinyi <akinyiglory2@gmail.com>
2025-02-24 13:05:05 +0300

    initial commit

--- views.py
+++ views.py
@@ -39,5 +39,15 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/child')
+def child():
+    return render_template('child.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
